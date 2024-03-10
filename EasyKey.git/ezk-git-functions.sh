71b3f4654ff7c37d5e37337bcb381e7f470c816a
Niklas Schlimm <nschlimm@timocom.com>
2024-03-10 16:57:01 +0100

    Refactoring

---
+++ EasyKey.git/ezk-git-functions.sh
@@ -0,0 +1,372 @@
+#!/bin/bash
+
+function analyzeWorkingDir (){
+   wstat=$(git diff HEAD --shortstat) # analyze local dir working status vs. actual checkout
+   if [ -z "$wstat" ]; then
+      wstat="tracked files clean"
+   fi
+   untracked=$(git ls-files --others --exclude-standard)
+   if [ -z "$untracked" ]; then
+      wstat="$wstat (no untracked files present)"
+    else
+      filescount=$(git ls-files --others --exclude-standard | wc -l)
+      wstat="$wstat (WARN: $filescount untracked file(s) present)"
+   fi
+   echo "> $wstat"
+   echo
+}
+
+function pushActual() {
+  executeCommand "git fetch --all"
+  importantLog "Checking your head state"
+  if git status | grep -q "HEAD detached"; then
+     echo "... you seem to be on a detached head state ... can't push ..."
+  else
+    echo "... your HEAD is attached to $actual ..."
+    mergeChanges
+    addAllUntracked
+    commitChanges
+    pushChanges
+  fi
+}
+
+function pushChanges () {
+    importantLog "Checking for stuff to push to origin/$actual"
+    if git log origin/$actual..$actual --oneline | grep -q ".*"; then
+      echo "... found commited updates in $actual waiting for push to origin/$actual ..."
+      read -p "Push (y/n)? " -n 1 -r
+      echo
+      if [[ $REPLY =~ ^[Yy]$ ]]; then #push
+         executeCommand "git push -u origin $actual"
+      fi
+    else
+      echo "... nothing to push ..."
+    fi
+}
+
+function addAllUntracked () {
+    importantLog "Checking for untracked files in the working tree"
+    # check to see if untracked files are in working tree
+    if git ls-files --others --exclude-standard | grep -q ".*"; then
+      echo "... untracked files found ..."
+      executeCommand "git ls-files --others --exclude-standard"
+      read -p "Add all (y/n)? " -n 1 -r
+      echo    # (optional) move to a new line
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+         executeCommand "git add ."
+      fi
+    else
+      echo "... no untracked files present ..."
+    fi
+}
+
+function mergeChanges () {
+    importantLog "Checking for updates from origin/$actual"
+    if git diff $actual origin/$actual | grep -q ".*"; then
+       echo "... found diff between $actual and origin/$actual ..."
+       if git status | grep "Your branch is ahead"; then
+          echo "... your local branch is ahead of origin/$actual ... nothing to merge"
+        else
+          echo "... your local branch is behind of origin/$actual ... recommend merge !"
+          diffDrillDownAdvanced "git diff --name-status $actual origin/$actual" " .*" "$actual" "origin/$actual"
+          executeCommand "git diff --name-status $actual origin/$actual"
+          read -p "Merge (y/n)? " -n 1 -r
+          echo    # (optional) move to a new line
+          if [[ $REPLY =~ ^[Yy]$ ]]; then
+             executeCommand "git merge origin/$actual"
+          fi
+        fi
+    else
+       echo "... nothing to merge ... up to date"
+    fi
+}
+
+function commitChanges () {
+    importantLog "Checking for stuff to commit from the working tree"
+    if git status -s -uno| grep -q ".*"; then
+      echo "... found updates on tracked files in working tree ..."
+      diffDrillDownAdvanced "git status -s -uno" " .*" HEAD
+      read -p "Commit the updates (y/n)? " -n 1 -r
+      echo    # (optional) move to a new line
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+           read -p "Enter commit message:" cmsg
+           executeCommand "git commit -am '${cmsg}'" # stage and commit all tracked filess
+      fi
+    else
+      echo "... nothing to commit ..."
+    fi
+}
+
+function adminRemotes() {
+              echo
+            echo "Administer remotes:"
+            echo "a. Show remotes"
+            echo "b. Add remote"
+            echo "c. Inspect remote"
+            echo
+            read -p "Make your choice: " -n 1 -r subreply
+            echo
+
+            case $subreply in
+                "a")
+                   echo $'\nKnown remotes:'
+                   git remote -v
+                ;;
+                "b")
+                    echo "What is the adress?"
+                    read adress
+                    echo "Type an alias for this remote?"
+                    read ralias
+                    git remote add $ralias $adress
+                ;;
+                "c")
+                    git remote -v
+                    echo "Name of the remote to inspect"
+                    read rname
+                    git remote show $rname
+                ;;
+            esac
+
+}
+
+function showRepoHisto() {
+            git log --pretty=format:'%Cred%h%Creset | %Cgreen%ad%Creset | %s %C(yellow)%d%Creset %C(bold blue)[%an]%Creset %Cgreen(%cr)%Creset' --graph --date=short --all
+}
+
+function cloneRemote() {
+            echo "Where?"
+            . ~/Personal/fl.sh
+            pwd
+            echo "Remote repository url:"
+            read url
+            git clone $url
+}
+
+function newLocalBranch() {
+              echo "Name des neuen Branch?"
+            read branchname
+            git branch $branchname 
+            git checkout $branchname
+            read -p "Set upstream? " -n 1 -r
+            echo    # (optional) move to a new line
+            if [[ $REPLY =~ ^[Yy]$ ]]
+                then
+                   git push --set-upstream origin $branchname
+            fi      
+}
+
+function pushLocalBranch() {
+    echo "Name des neuen Branch?"
+    read branchname
+    [ "${branchname}" = "" ] && waitonexit && return 
+    git branch $branchname 
+    git checkout $branchname
+    git git push -u origin $branchname
+}
+
+function rollBackLast() {
+            read -p "This rolls back one commit. Continue? [y/n]" -n 1 -r
+            if [[ $REPLY =~ ^[Yy]$ ]]
+                then
+                  git reset HEAD~1
+            fi
+            git status -s
+            read -p "Delete file modifications in local working dir? " -n 1 -r
+            echo    # (optional) move to a new line
+            if [[ $REPLY =~ ^[Yy]$ ]]
+                then
+                    git reset HEAD --hard
+            fi      
+}
+
+function deleteBranch() {
+              git branch
+            echo "Welchen Branch löschen?"
+            read dbranch
+            git branch -d $dbranch
+            read -p "Delete remote? " -n 1 -r
+            echo    # (optional) move to a new line
+            if [[ $REPLY =~ ^[Yy]$ ]]
+                then
+                   git push origin --delete $dbranch
+            fi 
+}
+
+function mergeSourceToTarget(){
+              git branch --all
+            echo "Enter merge target branch"
+            read target
+            echo "Enter merge source branch"
+            read bsource
+            git checkout $target
+            git merge $bsource
+}
+
+function showAllBranches () {
+              git branch --all
+}
+
+function showBranchHisto(){
+   git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+}
+
+function setUpstream() {
+   git push --set-upstream origin $actual
+}
+
+function localGitConfig() {
+            vim .git/config
+}
+
+function globalGitConfig() {
+            vim ~/.gitconfig
+}
+
+function adminAliases() {
+            echo $'\nActual aliases:'
+            git config --get-regexp alias
+            read -p "Add or delete aliases (a/d)? " -n 1 -r
+            echo    # (optional) move to a new line
+            if [[ $REPLY =~ ^[a]$ ]]
+                then
+                    echo "Which command?"
+                    read bcommand
+                    echo "Define alias:"
+                    read calias
+                    git config --global alias.$calias $bcommand
+                    echo "Alias $calias create for $bcommand!"
+                else
+                    echo "Which alias to delete:"
+                    read calias
+                    git config --global --unset alias.$calias
+            fi      
+}
+
+function gitIgnore() {
+            vim .gitignore
+}
+
+function undoReset () {
+  git reflog --date=iso
+  echo "Choose commit to reset:"
+  read cname
+  if [ -z "$cname" ]; then
+    echo "No commit entered!"
+  else
+    git reset $cname
+  fi
+}
+
+function interactiveStage () {
+   git add -i
+}
+
+function changeBranch () {
+       git branch --all
+       echo "Which branch?"
+       read bname
+       git checkout $bname
+}
+
+function fetachAll () {
+       git fetch --prune
+       git fetch --all
+}
+
+# submenus
+
+function workingDiffs() {
+  source $script_dir/../EasyKey.git/diff.sh
+  nowaitonexit
+}
+
+function atlassiansView() {
+  source $script_dir/../EasyKey.git/atlassian.sh
+  nowaitonexit
+}
+
+function changeProject () {
+  source $script_dir/../EasyKey.git/fl.sh
+  nowaitonexit
+}
+
+function gitExtras () {
+  source $script_dir/../EasyKey.git/gitExtras.sh
+  nowaitonexit
+}
+
+function gitPasswort () {
+  source $script_dir/../EasyKey.git/userpas.sh
+  nowaitonexit
+}
+
+function reset () {
+  executeCommand "git reset"
+}
+
+function commitAll () {
+    executeCommand "git commit -a"
+}
+
+function assumeUnchanged () {
+    git ls-files
+    echo "Which file?"
+    read filename
+    [ "${filename}" = "" ] && waitonexit && return 
+    executeCommand "git update-index --assume-unchanged $filename"
+}
+
+function unAssumeUnchanged () {
+    git ls-files -v | grep '^[[:lower:]]'
+    echo "Which file?"
+    read filename
+    executeCommand "git update-index --no-assume-unchanged $filename"
+}
+
+function coRemoteBranch () {
+       git branch -r
+       echo "Which remote branch?"
+       read bname
+       [ "${bname}" = "" ] && waitonexit && return 
+       git checkout --track $bname 
+}
+
+function setRemoteOrigin() {
+   echo "Enter remot origin address:"
+   read originaddress
+   executeCommand "git remote set-url origin $originaddress"
+}
+
+function showStatus () {
+  importantLog $(pwd | grep -o "[^/]*$")
+  actual=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+  importantLog $actual 
+  git log --decorate --oneline -n 1
+  git status | grep "Your branch"
+  analyzeWorkingDir
+  git remote -v
+}
+
+diffDrillDownAdvanced () { 
+  listkommando="$1"
+  regexp="$2"
+  if $listkommando | grep -q ".*"; then
+   while true; do
+        importantLog "Drill down into file logcommanddiff: $listkommando"
+        selectItem "$listkommando" "$regexp"
+        if [[ $fname = "" ]]; then
+          break
+        fi
+        if [ $# -eq 3 ]
+          then
+             kommando="git difftool $3 -- $fname"
+             executeCommand "$kommando"
+        fi
+        if [ $# -eq 4 ]
+          then
+             kommando="git difftool $3 $4 -- $fname"
+             executeCommand "$kommando"
+        fi
+   done
+  fi
+}
---
+++ EasyKey.kubectl/ezk-kubectl-functions.sh
@@ -0,0 +1,221 @@
+#!/bin/bash
+
+function listImagesInUse() {
+
+    namespaces=("sls-alfred" "sls-pennyworth")
+
+    contexts=$(kubectl config get-contexts -o name)
+
+    for context in $contexts; do
+        kubectl config use-context $context
+
+        greenLog "Context: $context"
+        for ns in "${namespaces[@]}"; do
+            echo "Namespace: $ns"
+            for pod in $(kubectl get pods -n $ns -o jsonpath='{.items[*].metadata.name}'); do
+                kubectl describe pod $pod -n $ns | grep -E "Container ID|Image:" && echo ""
+            done
+        done
+    done
+
+}
+
+function switchContext() {
+    selectItem "kubectl config get-contexts" "awk '{print \$2}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl config use-context $fname"
+}
+
+function addCluster() {
+   echo "Cluster name (e.g. development)?"
+   read clusterName
+   echo "Cluster address (e.g. https://5.6.7.8)?"
+   read clusterAddress
+   executeCommand "kubectl config set-cluster $clusterName --server=$clusterAddress --insecure-skip-tls-verify"
+}
+
+function addUsers() {
+   echo "User name (e.g. admin)?"
+   read userName
+   echo "Token (e.g. bHVNUkxJZU82d0JudWtpdktBbzhDZFVuSDVEYWtiVmJua3RVT3orUkNzDFGH)?"
+   read userToken
+   executeCommand "kubectl config set-credentials $userName --token $userToken"
+}
+
+function addContext() {
+   echo "Context name (e.g. development-pennyworth)?"
+   read contextName
+   echo "Cluster name (e.g. development)?"
+   read clusterName
+   echo "Namespace (e.g. default)?"
+   read namspace
+   echo "User (e.g. admin)?"
+   read userName
+   executeCommand "kubectl config set-context $contextName --cluster=$clusterName --namespace=$namspace --user=$userName"
+}
+
+function showPodManifest() {
+    selectItem "kubectl get pods" "awk '{print \$1}'" 100 1 "2"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl get pods $fname -o yaml"
+}
+
+function describePod() {
+    selectItem "kubectl get pods" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl describe pods $fname"
+}
+
+function showDeploymentManifest() {
+    selectItem "kubectl get deployments" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl get deployment $fname -o yaml"
+}
+
+function describeDeployment() {
+    selectItem "kubectl get deployments" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl describe deployment $fname"
+}
+
+function showServiceManifest() {
+    selectItem "kubectl get svc" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl get svc $fname -o yaml"
+}
+
+function describeService() {
+    selectItem "kubectl get svc" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl describe svc $fname"
+}
+
+function describeReplicaset() {
+    selectItem "kubectl get rs" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl describe rs $fname"
+}
+
+function getPodLogs() {
+    selectItem "kubectl get pods" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl logs $fname"
+}
+
+function logOnPod() {
+    selectItem "kubectl get pods" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl exec -it $fname -- sh"
+}
+
+function logOnDb(){
+   selectItem "kubectl get pods" "awk '{print \$1}'"
+   if [[ $fname == "" ]]; then return 0; fi
+   echo "DB user name (e.g. testUser)?"
+   read userName
+   echo "DB name (e.g. testDB)?"
+   read dbName
+   executeCommand "kubectl exec -it $fname -- psql --host localhost --username $userName -d $dbName"
+}
+
+function switchNamespace() {
+    selectItem "kubectl get ns" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl config set-context --current --namespace $fname"
+}
+
+function applyPodManifest() {
+    selectItem "grep -r 'kind: Pod' . | cut -d':' -f1" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    cat $fname
+    read -p "Apply manifest to Kubernetes (y/n)? " -n 1 -r
+    echo    # (optional) move to a new line                    if [[ $REPLY =~ ^[Yy]$ ]]
+    if [[ $REPLY =~ ^[Yy]$ ]]
+     then
+      executeCommand "kubectl apply -f $fname"
+     fi
+}
+
+function deletePod() {
+    selectItem "kubectl get pods" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl delete pod $fname"
+}
+
+function createNamespace(){
+   echo "Namespace name (e.g. frontend)?"
+   read nsName
+   executeCommand "kubectl create ns $nsName"
+}
+
+function redeploy () {
+    selectItem "kubectl get deployments" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl rollout restart deployment $fname"
+}
+
+function editConfigMap () {
+    selectItem "kubectl get configmaps" "awk '{print \$1}'"
+    if [[ $fname == "" ]]; then return 0; fi
+    executeCommand "kubectl edit configmap $fname"
+}
+
+function rollbackDeployment () {
+   kubectl get deployments
+   echo
+   deplHist
+   echo
+   echo "Enter deployment name (e.g. pennyworth):"
+   read depname
+   echo "Select revision:"
+   read revision
+   executeCommand "kubectl rollout undo deployment $depname --to-revision=$revision"
+}
+
+print_result() {
+  # ANSI escape code for green text
+  GREEN='\033[0;32m'
+  # ANSI escape code to reset text color
+  RESET='\033[0m'
+
+  if [[ "$1" == *"successfully"* ]]; then
+    echo -e "${GREEN}$1${RESET}"
+  else
+    echo "$1"
+  fi
+}
+
+deployState() {
+  result=$(kubectl rollout status deploy)
+  print_result "$result"
+}
+
+deplHist() {
+   depname=$(kubectl get deployments --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+   kubectl rollout history deployment/$depname -o yaml | grep -E "revision|creationTimestamp|image:" | sed 's/^[ \t]*//' | awk '/revision/ {print "\033[44m\033[39m" $0 "\033[0m"} !/revision/ && !/creationTimestamp: null/ {print}'
+}
+
+deplomentActualStatus() {
+
+    # Get the list of pods in the current namespace
+    pod_list=$(kubectl get pods --namespace=$(kubectl config view --minify | grep namespace | awk '{print $2}') -o jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}')
+
+    # Initialize variables for status tracking
+    all_pods_running=true
+
+    # Loop through each line of the pod list
+    while IFS=$'\t' read -r pod_name pod_status; do
+      # Check if the pod is not in the "Running" state
+      if [[ "${pod_status}" != "Running" ]]; then
+        all_pods_running=false
+        break
+      fi
+    done <<< "${pod_list}"
+
+    if ${all_pods_running}; then
+      greenLog "All pods ready"
+    else
+      redLog "Not all pods ready"
+    fi
+
+}
