910af89dae52f46217beff9cb8a18f2ecc320e29
Niklas Schlimm <nschlimm@timocom.com>
2024-03-23 12:40:38 +0100

    Test change

--- EasyKey.git/ezk-git-diff.sh
+++ EasyKey.git/ezk-git-diff.sh
@@ -96,7 +96,7 @@ menuItem a "actual branch        vs. origin/actual branch.      -> local reposit
 menuItem b "actual working dir   vs. actual branch last commit  -> tree vs. local repository" dirHead
 menuItem c "actual working dir   vs. other commits              -> tree vs. local repository" treeCommit
 menuItem d "commit               vs. commit                     -> local repository vs. local repository" commitCommit
-menuItem e "branch               vs. branch                     -> repository vs. repository" branchBranch
+menuItem e "branch               vs. branch                     -> repository vs. repository " branchBranch
 echo
 submenuHead "Specific diffs:"
 menuItem k "Diff since date" diffDate
