cdf2c6dc23074d57007ab190b9bc65b77fd444df
Niklas Schlimm <nschlimm@timocom.com>
2024-03-25 15:18:11 +0100

    Log in cherry pick process changed

--- EasyKey.git/ezk-git-atln.sh
+++ EasyKey.git/ezk-git-atln.sh
@@ -26,7 +26,7 @@ function mergeRebase () {
 
 cherryPick() {
    echo "Last 15 commits"
-   git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -n 100
+   git log --all --oneline
    echo "Enter commit you want to pick:"
    read cname
    [ "${cname}" = "" ] && waitonexit && return 
